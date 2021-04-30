4d6f3498565c12caf95b8ec5a536da77ab7fa4a6
unkulunkului <xc.prsj@gmail.com>
2021-04-30 15:24:18 +0300

    Initial commit

--- src/test/java/by/tms/diploma/service/impl/RequestServiceImplTest.java
+++ src/test/java/by/tms/diploma/service/impl/RequestServiceImplTest.java
@@ -6,112 +6,126 @@ import by.tms.diploma.entity.Hotel;
 import by.tms.diploma.repository.RequestRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@SpringBootTest
 class RequestServiceImplTest {
 
 
-    @Autowired
+    @MockBean
     private RequestRepository repository;
 
+
+    @Autowired
+    private RequestServiceImpl requestService;
+
     private ClientRequest clientRequest;
+
+    private final String FIRST_REQUEST_NAME = "First name";
+    private final String SECOND_REQUEST_NAME = "Second name";
+    private final String THIRD_REQUEST_NAME = "Third name";
+
+    private final long FIRST_REQUEST_CURATOR_ID = 122L;
+    private final long SECOND_REQUEST_CURATOR_ID = 61L;
+    private final long THIRD_REQUEST_CURATOR_ID = 95L;
+
+    private final long FIRST_REQUEST_ID = 1L;
+
+    private final long FIRST_REQUEST_NEW_CURATOR_ID = 11L;
+
+    private final ClientRequestStatusEnum FIRST_REQUEST_OLD_STATUS = ClientRequestStatusEnum.WAITING;
+    private final ClientRequestStatusEnum FIRST_REQUEST_NEW_STATUS = ClientRequestStatusEnum.DONE;
+
+
 
     @BeforeEach
     void setUp() {
         clientRequest = new ClientRequest();
-        clientRequest.setName("Name");
-        clientRequest.setCuratorId(129);
-        clientRequest.setRequestStatus(ClientRequestStatusEnum.IN_PROGRESS);
+        clientRequest.setName(FIRST_REQUEST_NAME);
+        clientRequest.setCuratorId(FIRST_REQUEST_CURATOR_ID);
     }
 
     @Test
     void save() {
-        ClientRequest expected = clientRequest;
-        ClientRequest actual = repository.save(clientRequest);
-        assertEquals(expected, actual);
+        Mockito.when(repository.save(clientRequest)).thenReturn(clientRequest);
+        ClientRequest actual = requestService.save(clientRequest);
+        assertEquals(clientRequest, actual);
     }
 
     @Test
     void getAllRequest() {
         ClientRequest secondRequest = new ClientRequest();
-        secondRequest.setName("second name");
+        secondRequest.setName(SECOND_REQUEST_NAME);
+        secondRequest.setCuratorId(SECOND_REQUEST_CURATOR_ID);
+
         ClientRequest thirdRequest = new ClientRequest();
-        thirdRequest.setName("third name");
-        ClientRequest [] expected = new ClientRequest[3];
-        expected[0] = (clientRequest);
-        expected[1] = (secondRequest);
-        expected[2] = (thirdRequest);
-        List<ClientRequest> clientRequests = repository.saveAll(Arrays.asList(expected));
-        ClientRequest[] actual = clientRequests.toArray(new ClientRequest[0]);
-        assertArrayEquals(expected, actual);
+        thirdRequest.setName(THIRD_REQUEST_NAME);
+        thirdRequest.setCuratorId(THIRD_REQUEST_CURATOR_ID);
+
+        List<ClientRequest> clientRequests = new ArrayList<>();
+        clientRequests.add(thirdRequest);
+        clientRequests.add(secondRequest);
+        clientRequests.add(clientRequest);
+
+        Mockito.when(requestService.getAllRequest()).thenReturn(clientRequests);
+
+        List<ClientRequest> allRequest = requestService.getAllRequest();
+        assertEquals(clientRequests.size(), allRequest.size());
     }
 
     @Test
     void findById() {
-        ClientRequest expected = clientRequest;
-        ClientRequest save = repository.save(expected);
-        long id = save.getId();
-        Optional<ClientRequest> clientRequest = repository.findById(id);
+        Mockito.when(repository.findById(FIRST_REQUEST_ID)).thenReturn(Optional.of(clientRequest));
+        Optional<ClientRequest> requestFromRepository = requestService.findById(1L);
         ClientRequest actual = new ClientRequest();
-        if (clientRequest.isPresent()) {
-            actual = clientRequest.get();
+        if (requestFromRepository.isPresent()) {
+            actual = requestFromRepository.get();
         }
-        assertEquals(expected, actual);
+        assertEquals(clientRequest, actual);
     }
 
     @Test
     void updateStatusById() {
-        ClientRequest save = repository.save(clientRequest);
+        Mockito.when(repository.findById(FIRST_REQUEST_ID)).thenReturn(Optional.of(clientRequest));
+        Mockito.spy(repository).save(clientRequest);
 
-        long id = save.getId();
-        ClientRequestStatusEnum expected = ClientRequestStatusEnum.DONE;
-
-        Optional<ClientRequest> clientRequest = repository.findById(id);
-        if (clientRequest.isPresent()) {
-            ClientRequest fromRepository = clientRequest.get();
-            fromRepository.setRequestStatus(expected);
-            repository.save(fromRepository);
+        requestService.updateStatusById(FIRST_REQUEST_ID, FIRST_REQUEST_NEW_STATUS);
+        Optional<ClientRequest> byId = requestService.findById(FIRST_REQUEST_ID);
+        ClientRequestStatusEnum statusEnum = FIRST_REQUEST_OLD_STATUS;
+        if (byId.isPresent()) {
+            ClientRequest clientRequestFromRepository = byId.get();
+            statusEnum = clientRequestFromRepository.getRequestStatus();
         }
 
-        ClientRequestStatusEnum actual = ClientRequestStatusEnum.WAITING;
-        Optional<ClientRequest> changedClientRequest = repository.findById(id);
-        if (changedClientRequest.isPresent()) {
-            ClientRequest fromRepository = clientRequest.get();
-            actual = fromRepository.getRequestStatus();
-        }
-
-        assertEquals(expected, actual);
+        assertEquals(FIRST_REQUEST_NEW_STATUS, statusEnum);
     }
 
     @Test
     void setCuratorIdById() {
-        ClientRequest save = repository.save(clientRequest);
+        Mockito.when(repository.findById(FIRST_REQUEST_ID)).thenReturn(Optional.of(clientRequest));
+        Mockito.spy(repository).save(clientRequest);
 
-        long id = save.getId();
-        long expected = 12;
-
-        Optional<ClientRequest> clientRequest = repository.findById(id);
-        if (clientRequest.isPresent()) {
-            ClientRequest fromRepository = clientRequest.get();
-            fromRepository.setCuratorId(expected);
-            repository.save(fromRepository);
+        requestService.setCuratorIdById(FIRST_REQUEST_ID, FIRST_REQUEST_NEW_CURATOR_ID);
+        Optional<ClientRequest> byId = requestService.findById(FIRST_REQUEST_ID);
+        long requestCuratorId = FIRST_REQUEST_CURATOR_ID;
+        if (byId.isPresent()) {
+            ClientRequest clientRequestFromRepository = byId.get();
+            requestCuratorId = clientRequestFromRepository.getCuratorId();
         }
-
-        long actual = 0;
-        Optional<ClientRequest> changedClientRequest = repository.findById(id);
-        if (changedClientRequest.isPresent()) {
-            ClientRequest fromRepository = clientRequest.get();
-            actual = fromRepository.getCuratorId();
-        }
-
-        assertEquals(expected, actual);
+        assertEquals(FIRST_REQUEST_NEW_CURATOR_ID, requestCuratorId);
     }
 }
